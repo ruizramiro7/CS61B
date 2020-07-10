670df324db98f2968ea49d3a806352730ab31d42
Brandon Bizzarro <bbbizzarro@gmail.com>
2020-07-10 16:29:08 -0700

    Completed lab09

--- lab09/capers/Dog.java
+++ lab09/capers/Dog.java
@@ -6,10 +6,10 @@ import java.io.Serializable;
 /** Represents a dog that can be serialized.
  * @author Sean Dooher
 */
-public class Dog { // FIXME
+public class Dog implements Serializable{ // FIXME
 
     /** Folder that dogs live in. */
-    static final File DOG_FOLDER = null; // FIXME
+    static final File DOG_FOLDER = Utils.join(Main.CAPERS_FOLDER, "dogs"); // FIXME
 
     /**
      * Creates a dog object with the specified parameters.
@@ -31,7 +31,7 @@ public class Dog { // FIXME
      */
     public static Dog fromFile(String name) {
         // FIXME
-        return null;
+        return Utils.readObject(Utils.join(DOG_FOLDER, name), Dog.class);
     }
 
     /**
@@ -48,6 +48,8 @@ public class Dog { // FIXME
      */
     public void saveDog() {
         // FIXME
+        File outFile = Utils.join(DOG_FOLDER, _name);
+        Utils.writeContents(outFile, Utils.serialize(this));
     }
 
     @Override
--- lab09/capers/Main.java
+++ lab09/capers/Main.java
@@ -1,42 +1,56 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
-/** Canine Capers: A Gitlet Prelude.
+/**
+ * Canine Capers: A Gitlet Prelude.
+ *
  * @author Sean Dooher
-*/
+ */
 public class Main {
-    /** Current Working Directory. */
+    /**
+     * Current Working Directory.
+     */
     static final File CWD = new File(".");
 
-    /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // FIXME
+    /**
+     * Main metadata folder.
+     */
+    //static final File CAPERS_FOLDER = new File(CWD.getPath() + "/.capers"); // FIXME
+    static final File CAPERS_FOLDER = Utils.join(CWD, ".capers"); // FIXME
+
+    /**
+     * Main story file
+     */
+    static final File STORY_FILE = Utils.join(CAPERS_FOLDER, "story");
 
     /**
      * Runs one of three commands:
      * story [text] -- Appends "text" + a newline to a story file in the
-     *                 .capers directory. Additionally, prints out the
-     *                 current story.
-     *
+     * .capers directory. Additionally, prints out the
+     * current story.
+     * <p>
      * dog [name] [breed] [age] -- Persistently creates a dog with
-     *                             the specified parameters; should also print
-     *                             the dog's toString(). Assume dog names are
-     *                             unique.
-     *
+     * the specified parameters; should also print
+     * the dog's toString(). Assume dog names are
+     * unique.
+     * <p>
      * birthday [name] -- Advances a dog's age persistently
-     *                    and prints out a celebratory message.
-     *
+     * and prints out a celebratory message.
+     * <p>
      * All persistent data should be stored in a ".capers"
      * directory in the current working directory.
-     *
+     * <p>
      * Recommended structure (you do not have to follow):
-     *
+     * <p>
      * *YOU SHOULD NOT CREATE THESE MANUALLY,
-     *  YOUR PROGRAM SHOULD CREATE THESE FOLDERS/FILES*
-     *
+     * YOUR PROGRAM SHOULD CREATE THESE FOLDERS/FILES*
+     * <p>
      * .capers/ -- top level folder for all persistent data in your lab12 folder
-     *    - dogs/ -- folder containing all of the persistent data for dogs
-     *    - story -- file containing the current story
+     * - dogs/ -- folder containing all of the persistent data for dogs
+     * - story -- file containing the current story
      *
      * @param args arguments from the command line
      */
@@ -46,12 +60,18 @@ public class Main {
         }
         setupPersistence();
         switch (args[0]) {
-        case "story":
-            writeStory(args);
-            break;
-        // FIXME
-        default:
-            exitWithError(String.format("Unknown command: %s", args[0]));
+            case "story":
+                writeStory(args);
+                break;
+            case "dog":
+                makeDog(args);
+                break;
+            case "birthday":
+                celebrateBirthday(args);
+                break;
+            // FIXME
+            default:
+                exitWithError(String.format("Unknown command: %s", args[0]));
         }
         return;
     }
@@ -60,24 +80,40 @@ public class Main {
      * Does required filesystem operations to allow for persistence.
      * (creates any necessary folders or files)
      * Remember: recommended structure (you do not have to follow):
-     *
+     * <p>
      * .capers/ -- top level folder for all persistent data in your lab12 folder
-     *    - dogs/ -- folder containing all of the persistent data for dogs
-     *    - story -- file containing the current story
-     *
+     * - dogs/ -- folder containing all of the persistent data for dogs
+     * - story -- file containing the current story
      */
     public static void setupPersistence() {
         // FIXME
+        if (!CAPERS_FOLDER.exists()) {
+            CAPERS_FOLDER.mkdir();
+        }
+
+        if (!Dog.DOG_FOLDER.exists()) {
+            Dog.DOG_FOLDER.mkdir();
+        }
+        try {
+            STORY_FILE.createNewFile();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * Appends the first non-command argument in args
      * to a file called `story` in the .capers directory.
+     *
      * @param args Array in format: {'story', text}
      */
     public static void writeStory(String[] args) {
         validateNumArgs("story", args, 2);
         // FIXME
+        String contents = Utils.readContentsAsString(STORY_FILE) + args[1] + "\n";
+        Utils.writeContents(STORY_FILE, contents);
+        System.out.println(contents);
     }
 
     /**
@@ -85,11 +121,15 @@ public class Main {
      * three non-command arguments of args (name, breed, age).
      * Also prints out the dog's information using toString().
      * If the user inputs an invalid date, call exitWithError()
+     *
      * @param args Array in format: {'story', name, breed, age}
      */
     public static void makeDog(String[] args) {
         validateNumArgs("dog", args, 4);
         // FIXME
+        Dog dog = new Dog(args[1], args[2], Integer.parseInt(args[3]));
+        dog.saveDog();
+        System.out.println(dog.toString());
     }
 
     /**
@@ -97,19 +137,24 @@ public class Main {
      * Also prints out the dog's information using toString().
      * Chooses dog to advance based on the first non-command argument of args.
      * If the user's input is invalid, call exitWithError()
+     *
      * @param args Array in format: {'birthday', name}
      */
     public static void celebrateBirthday(String[] args) {
         validateNumArgs("birthday", args, 2);
         // FIXME
+        Dog dog = Dog.fromFile(args[1]);
+        dog.haveBirthday();
+        dog.saveDog();
     }
 
     /**
      * Prints out MESSAGE and exits with error code -1.
      * Note:
-     *     The functionality for erroring/exit codes is different within Gitlet
-     *     so DO NOT use this as a reference.
-     *     Refer to the spec for more information.
+     * The functionality for erroring/exit codes is different within Gitlet
+     * so DO NOT use this as a reference.
+     * Refer to the spec for more information.
+     *
      * @param message message to print
      */
     public static void exitWithError(String message) {
@@ -123,14 +168,14 @@ public class Main {
      * Checks the number of arguments versus the expected number,
      * throws a RuntimeException if they do not match.
      *
-     * @param cmd Name of command you are validating
+     * @param cmd  Name of command you are validating
      * @param args Argument array from command line
-     * @param n Number of expected arguments
+     * @param n    Number of expected arguments
      */
     public static void validateNumArgs(String cmd, String[] args, int n) {
         if (args.length != n) {
             throw new RuntimeException(
-                String.format("Invalid number of arguments for: %s.", cmd));
+                    String.format("Invalid number of arguments for: %s.", cmd));
         }
     }
 }
